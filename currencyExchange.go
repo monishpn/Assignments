5b331b1b44dc49ff43e49346c6c9c08ce1012f8d
monishpn <monish.pn@zop.dev>
2025-06-12 10:18:54 +0530

    CurrencyExchange-2

--- currencyExchange.go
+++ currencyExchange.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func exchange(a float32, cur, tgt string) float32 {
@@ -20,9 +21,16 @@ func exchange(a float32, cur, tgt string) float32 {
 		a *= jpy
 	} else if cur == "EUR" {
 		a *= eur
+	} else if cur == "INR" {
+		a *= 1
+	} else {
+		fmt.Printf("Invalid currency value, %s not in the databse", cur)
+		os.Exit(0)
 	}
 
-	if tgt == "USD" {
+	if tgt == "INR" {
+		return a
+	} else if tgt == "USD" {
 		return a / usd
 	} else if tgt == "JPY" {
 		return a / jpy
@@ -30,13 +38,25 @@ func exchange(a float32, cur, tgt string) float32 {
 		return a / eur
 	}
 
-	return a
+	fmt.Printf("Invalid currency value, %s not in the databse", tgt)
+	os.Exit(0)
+	return 0
 
 }
 
 func main() {
 	var a float32
 	var cur, tgt string
+
+	now := time.Now().Hour()
+
+	if now >= 6 && now < 12 {
+		fmt.Println("Good Morning!!!!")
+	} else if now >= 12 && now < 16 {
+		fmt.Println("Good Afternoon!!!")
+	} else {
+		fmt.Println("Good Evening!!!")
+	}
 
 	fmt.Print("Enter the amount : ")
 	_, errA := fmt.Scanln(&a)
