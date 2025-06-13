fd30ec027204653b5650e995917f6381c6f4bc8f
monishpn <monish.pn@zop.dev>
2025-06-13 12:01:56 +0530

    Final File Health

--- Assignment-2/FHealth.go
+++ Assignment-2/FHealth.go
@@ -4,9 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go FILE")
+		os.Exit(1)
+	}
 
 	fileName := os.Args[1]
 
@@ -19,29 +24,43 @@ func main() {
 	info := 0
 	er := 0
 	war := 0
-
-	var strArr []string
+	//
+	var strArr string
 
 	defer file.Close()
 
 	Scanner := bufio.NewScanner(file)
-	Scanner.Split(bufio.ScanWords)
 	for Scanner.Scan() {
-		strArr = append(strArr, Scanner.Text())
-	}
-
-	for _, word := range strArr {
-		switch word {
-		case "[INFO]":
-			info += 1
-		case "[WARNING]":
-
-			war += 1
-		case "[ERROR]":
-			er += 1
-
+		strArr = Scanner.Text()
+		switch {
+		case strings.HasPrefix(strArr, "[INFO]"):
+			info++
+		case strings.HasPrefix(strArr, "[WARNING]"):
+			war++
+		case strings.HasPrefix(strArr, "[ERROR]"):
+			er++
 		}
+
 	}
 
+	//Scanner.Split(bufio.ScanWords)
+	//for Scanner.Scan() {
+	//	strArr = append(strArr, Scanner.Text())
+	//}
+	//
+	//for _, word := range strArr {
+	//	switch word {
+	//	case "[INFO]":
+	//		info += 1
+	//	case "[WARNING]":
+	//
+	//		war += 1
+	//	case "[ERROR]":
+	//		er += 1
+	//
+	//	}
+	//}
+	//
 	fmt.Printf("Log Analysis of File : %s \n\n INFO : %d\n WARNING : %d\n ERROR : %d\n\n  ", fileName, info, war, er)
+
 }
