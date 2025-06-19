9350f43c5349761aafb3feaa15424df2e6baf978
monishpn <monish.pn@zop.dev>
2025-06-19 11:18:31 +0530

    For multiple test cases

--- Assignment-6/task_test.go
+++ Assignment-6/task_test.go
@@ -1,49 +1,105 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 var msg = []string{
 	"Hello",
 	"World",
 	"How are you?",
 	"How was your day?",
+	"Today was a nice day",
+	"Yeah it was a nice day",
+	"Bring in the Energy",
+	"Should have done it today",
 }
 
-func Test_create(t *testing.T) {
-	exp := 3
-	i := 0
-	for i = 0; i < exp; i++ {
-		CreateTask(i, msg[i])
-	}
-
-	op := len(m)
-
-	op_msg := m[op-1]
-
-	if op_msg != msg[exp-1] {
-		t.Errorf("Expected: %v got: %v", msg[exp-1], op_msg)
-	}
-
-	if op != exp {
-		t.Errorf("createTask failed, exp %d, got %d", exp, op)
-	}
-
+type test struct {
+	step    []byte
+	exp_len int
 }
 
-func TestDeleteTask(t *testing.T) {
-	exp := 3
+var tests = []test{
+	{[]byte{'c', 'c', 'c'}, 3},
+	{[]byte{'c', 'c', 'd'}, 1},
+	{[]byte{'c', 'c', 'd', 'd'}, 0},
+	{[]byte{'c', 'c', 'c', 'd'}, 2},
+}
 
-	for i := 0; i < exp; i++ {
-		CreateTask(i, msg[i])
-	}
-	deleteTask(exp)
-	op := len(m)
-	op_msg := m[op-1]
+func Test(t *testing.T) {
+	var i int = 0
+	for test_case, T := range tests {
+		i = 0
 
-	if op_msg != msg[exp-1] {
-		t.Errorf("Expected: %v got: %v", msg[exp-1], op_msg)
-	}
-	if op != exp {
-		t.Errorf("deleteTask failed, exp %d, got %d", exp, op)
+		for _, operation := range T.step {
+
+			if operation == 'c' {
+
+				if i != 0 && T.step[i-1] == 'd' {
+					i++
+				}
+
+				CreateTask(i, msg[i])
+				i++
+			}
+
+			if operation == 'd' {
+				i = i - 1
+				deleteTask(i)
+			}
+		}
+
+		if T.exp_len != len(m) {
+			t.Errorf("%d : expected %v, got %v", test_case+1, T.exp_len, len(m))
+		}
+
+		if T.exp_len != 0 && msg[T.exp_len-1] != msg[len(m)-1] {
+			t.Errorf("%d : expected %v, got %v", test_case+1, msg[T.exp_len-1], msg[len(m)-1])
+		}
+
+		for len(m) != 0 {
+			deleteTask(len(m) - 1)
+		}
+
 	}
 }
+
+//func Test_create(t *testing.T) {
+//	exp := 3
+//	i := 0
+//	for i = 0; i < exp; i++ {
+//		CreateTask(i, msg[i])
+//	}
+//
+//	op := len(m)
+//
+//	op_msg := m[op-1]
+//
+//	if op_msg != msg[exp-1] {
+//		t.Errorf("Expected: %v got: %v", msg[exp-1], op_msg)
+//	}
+//
+//	if op != exp {
+//		t.Errorf("createTask failed, exp %d, got %d", exp, op)
+//	}
+//
+//}
+//
+//func TestDeleteTask(t *testing.T) {
+//	exp := 3
+//
+//	for i := 0; i < exp; i++ {
+//		CreateTask(i, msg[i])
+//	}
+//	deleteTask(exp)
+//	op := len(m)
+//	op_msg := m[op-1]
+//
+//	if op_msg != msg[exp-1] {
+//		t.Errorf("Expected: %v got: %v", msg[exp-1], op_msg)
+//	}
+//	if op != exp {
+//		t.Errorf("deleteTask failed, exp %d, got %d", exp, op)
+//	}
+//}
